d5b42a6aba91869f8e73985527392fb1791bde6d
doridori <lindak2004@naver.com>
2025-05-22 20:46:03 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg"
+    "img/0.jpg",
+    "img/1.jpg",
+    "img/2.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
